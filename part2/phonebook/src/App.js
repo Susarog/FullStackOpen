20a1ce5be084f6a96f752086fc299455c646df2c
Ryan Hang <ryanhang92802@gmail.com>
2022-07-19 21:45:37 -0700

    Finish 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,16 +1,15 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas",id: 0 }]);
+  const [persons, setPersons] = useState([{ name: "Arto Hellas",id: 0, number: "123-1231" }]);
   const [newName, setNewName] = useState("");
-
+  const [newNumber, setNewNumber] = useState("");
   const newInput = (event) => {
     setNewName(event.target.value);
   };
-
-
-  
-
+  const newInputNum = (event) => {
+    setNewNumber(event.target.value);
+  }
   const submit = (event) => {
     event.preventDefault();
     if(persons.map(person => person.name).includes(newName)){
@@ -18,7 +17,7 @@ const App = () => {
       setNewName('');
       return;
     }
-    setPersons(persons.concat({name: newName, id: persons.length}));
+    setPersons(persons.concat({name: newName, id: persons.length, number: newNumber}));
     setNewName('');
   }
   return (
@@ -29,13 +28,16 @@ const App = () => {
           name: <input value={newName} onChange={newInput} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={newInputNum} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <div>
         {persons.map((person) => (
-          <div key={person.id}>{person.name}</div>
+          <div key={person.id}>{person.name} {person.number}</div>
         ))}
       </div>
     </div>
