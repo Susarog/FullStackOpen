23b0ed8cdb7fb353f5d0287e7bacce77986e0d10
Ryan Hang <ryanhang92802@gmail.com>
2022-07-20 21:09:20 -0700

    Finish 2.13

--- part2/dataforcountries/src/App.js
+++ part2/dataforcountries/src/App.js
@@ -2,20 +2,40 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 
 const Countries = ({ countries, input }) => {
+  if(input.length == 0) {
+    return;
+  }
   const lowerCase = input.toLowerCase();
   const filteredCountries = countries
-    .map((name) => name.toLowerCase())
-    .filter((name) => name.includes(lowerCase));
+    .filter((obj) => obj.name.common.toLowerCase().includes(lowerCase));
   if (filteredCountries.length > 10) {
     return <div>Too many matches, specify another another filter</div>;
+  } else if (filteredCountries.length > 1) {
+    return (
+      <div>
+        {filteredCountries.map((name) => (
+          <div key={name.area}>{name.name.common}</div>
+        ))}
+      </div>
+    );
+  } else if (filteredCountries.length == 1) {
+    const country = filteredCountries[0];
+    let id = 1;
+    const countryVals = Object.values(country.languages)
+    return (
+      <div>
+          <h1>{country.name.common}</h1>
+          <div>capital: {country.capital}</div>
+          <div>area: {country.area}</div>
+
+          <h2>languages:</h2>
+          <ul>
+            {countryVals.map(language => <li key={id++}>{language}</li>)}
+          </ul>
+          <div>{country.flag}</div>
+      </div>
+    );
   }
-  return (
-    <div>
-      {filteredCountries.map((name) => (
-        <div>{name}</div>
-      ))}
-    </div>
-  );
 };
 
 const App = () => {
@@ -23,9 +43,9 @@ const App = () => {
   const [input, setNewInput] = useState("");
   useEffect(() => {
     axios.get("https://restcountries.com/v3.1/all").then((response) => {
-      setCountries(response.data.map((obj) => obj.name.common));
+      setCountries(response.data);
     });
-  },[]);
+  }, []);
 
   const filterInput = (event) => {
     setNewInput(event.target.value);
