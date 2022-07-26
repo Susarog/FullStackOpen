d7d0915fe53371106bf72bcad306f094e7649b35
Ryan Hang <ryanhang92802@gmail.com>
2022-07-25 22:54:14 -0700

    Finish 3.3

--- part3/index.js
+++ part3/index.js
@@ -35,6 +35,16 @@ app.get("/info", (req, res) => {
     } people</p><p>${new Date()}</p>`
   );
 });
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const personURL = phoneBookData.find((person) => person.id === id);
+  if (personURL) {
+    res.json(personURL);
+  } else {
+    res.status(404).end();
+  }
+});
 const PORT = 3001;
 
 app.listen(PORT, () => console.log(`Running port ${PORT}`));
