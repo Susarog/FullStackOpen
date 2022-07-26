431a03b0d64d37666a7bda88d373fb4038dc4d10
Ryan Hang <ryanhang92802@gmail.com>
2022-07-25 23:00:25 -0700

    Finish 3.4

--- part3/index.js
+++ part3/index.js
@@ -45,6 +45,13 @@ app.get("/api/persons/:id", (req, res) => {
     res.status(404).end();
   }
 });
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  phoneBookData = phoneBookData.filter((person) => person.id !== id);
+  res.status(204).end();
+});
+
 const PORT = 3001;
 
 app.listen(PORT, () => console.log(`Running port ${PORT}`));
