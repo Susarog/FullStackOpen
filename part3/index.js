0351c2a750eccd3d8ed2eed3e5a52c9d326c6d1f
Ryan Hang <ryanhang92802@gmail.com>
2022-07-28 19:06:27 -0700

    Finish 3.10

--- part3/index.js
+++ part3/index.js
@@ -101,6 +101,6 @@ app.post("/api/persons", (req, res) => {
   res.json(newPerson);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => console.log(`Running port ${PORT}`));
