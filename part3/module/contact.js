28b3ba3d2f7d711e4e82c891504d326f541a8448
Ryan Hang <ryanhang92802@gmail.com>
2022-07-31 17:08:05 -0700

    Finish 3.20

--- part3/module/contact.js
+++ part3/module/contact.js
@@ -17,9 +17,18 @@ const contactSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
-    minLength: 3
+    minLength: 3,
   },
-  number: String,
+  number: {
+    type: String,
+    validate: {
+      validator: function (v) {
+        return /^[0-9]{2,3}-[0-9]+/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+    required: [true, "User phone number required"],
+  },
 });
 
 contactSchema.set("toJSON", {
