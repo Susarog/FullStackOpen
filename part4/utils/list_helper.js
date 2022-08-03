f16ca03a7945c104c073cdb1e7a115ab83e9bbde
Ryan Hang <ryanhang92802@gmail.com>
2022-08-02 20:12:24 -0700

    Finish 4.4-4.5

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -3,6 +3,19 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const arrOfLikes = blogs.map((blog) => blog.likes)
+  return arrOfLikes.length === 0 ? 0 : arrOfLikes.reduce((sum,init) => sum + init)
+
+}
+
+const favoriteBlog = (blogs) => {
+  return blogs.length === 0 ? null : blogs.reduce((high, val) => high.likes > val.likes ? high : val)
+}
+
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  favoriteBlog
 }
