2e12c22e4aca15538239d4b59537745c4884c726
Ryan Hang <ryanhang92802@gmail.com>
2022-08-02 19:21:27 -0700

    Add Linter

--- part4/index.js
+++ part4/index.js
@@ -1,32 +1,10 @@
-const config = require("./utils/config");
-const logger = require("./utils/logger");
-const middleware = require("./utils/middleware");
-const blogsRouter = require('./controllers/blogs')
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const mongoose = require("mongoose");
+const app = require('./app')
+const http = require('http')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-logger.info("connecting to", config.MONGODB_URI);
+const server = http.createServer(app)
 
-mongoose
-  .connect(config.MONGODB_URI)
-  .then(() => {
-    logger.info("Connected to MongoDB");
-  })
-  .catch((err) => {
-    logger.info("Error connecting to MongoDB", err.message);
-  });
-
-app.use(cors());
-app.use(express.static('build'))
-app.use(express.json());
-app.use(middleware.requestLogger);
-app.use("/api/blogs",blogsRouter);
-app.use(middleware.errorHandler);
-app.use(middleware.unknownEndpoint);
-
-const PORT = config.PORT;
-app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`);
-});
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
+})
