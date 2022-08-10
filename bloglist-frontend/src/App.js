1006270b679f13952aa8a044178b6feaded07a45
Ryan Hang <ryanhang92802@gmail.com>
2022-08-09 23:04:18 -0700

    Finish 5.2

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -3,7 +3,6 @@ import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
 import Form from './components/Form'
-import login from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
@@ -17,18 +16,36 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogUser')
+    if(loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  },[])
+
   const handleLogin = async (event) => {
     event.preventDefault()
 
     try {
       const user = await loginService.login({username, password})
-
-      setUser(user)
+      window.localStorage.setItem(
+        'loggedBlogUser', JSON.stringify(user.data)
+      ) 
+      setUser(user.data)
       setUsername('')
       setPassword('')
-    } catch (exception){
+    } catch (exception) {
       console.log(exception)
     }
+  }
+  const handleLogout = (event) => {
+    event.preventDefault()
+    window.localStorage.removeItem('loggedBlogUser')
+    setUser(null)
+    setUsername('')
+    setPassword('')
   }
 
   if(user === null) {
@@ -39,14 +56,16 @@ const App = () => {
     return (
       <div>
         <h2>blogs</h2>
+        <div>
+        <p style={{display:"inline-block"}}>{user.username} logged in</p>
+        <button onClick={handleLogout}>logout</button>
+        </div>
         {blogs.map(blog =>
           <Blog key={blog.id} blog={blog} />
         )}
       </div>
     )
   }
-
-  
 }
 
 export default App
