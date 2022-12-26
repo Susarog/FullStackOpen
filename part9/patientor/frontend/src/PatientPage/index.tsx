523357ccd2e3db23db4bfdb756203073f2c3a591
Ryan Hang <ryanhang92802@gmail.com>
2022-12-26 15:39:52 -0800

    finish 9.22 but i kinda went lazy on this stuff ngl

--- part9/patientor/frontend/src/PatientPage/index.tsx
+++ part9/patientor/frontend/src/PatientPage/index.tsx
@@ -4,7 +4,9 @@ import { useStateValue } from '../state';
 import { useParams } from 'react-router-dom';
 import MaleIcon from '@mui/icons-material/Male';
 import FemaleIcon from '@mui/icons-material/Female';
-
+import MedicalServicesIcon from '@mui/icons-material/MedicalServices';
+import WorkIcon from '@mui/icons-material/Work';
+import LocalHospitalIcon from '@mui/icons-material/LocalHospital';
 const PatientPage = () => {
   const [{ patients, diagnoses }] = useStateValue();
   const { id } = useParams<{ id: string }>();
@@ -15,31 +17,78 @@ const PatientPage = () => {
     return (
       <div>
         <h2>
-          {patient.name}
+          {patient.name}{' '}
           {patient.gender === 'male' ? <MaleIcon /> : <FemaleIcon />}
         </h2>
         <div>ssn: {patient.ssn}</div>
         <div>occupation: {patient.occupation}</div>
         <h2>entries</h2>
         {patient.entries.map((entry) => {
-          return (
-            <div key={entry.id}>
-              <div>
-                {entry.date} {entry.description}
-              </div>
-              {entry.diagnosisCodes ? (
-                <ul>
-                  {entry.diagnosisCodes.map((code) => {
-                    return (
-                      <li key={code}>
-                        {code} {diagnoses[code].name}
-                      </li>
-                    );
-                  })}
-                </ul>
-              ) : null}
-            </div>
-          );
+          switch (entry.type) {
+            case 'Hospital':
+              return (
+                <div key={entry.id}>
+                  <div>{entry.date} <LocalHospitalIcon /></div>
+                  <div>{entry.description}</div>
+                  {entry.diagnosisCodes ? (
+                    <ul>
+                      {entry.diagnosisCodes.map((code) => {
+                        return (
+                          <li key={code}>
+                            {code} {diagnoses[code].name}
+                          </li>
+                        );
+                      })}
+                    </ul>
+                  ) : null}
+                  diagnose by {entry.specialist}
+                </div>
+              );
+            case 'OccupationalHealthcare':
+              return (
+                <div key={entry.id}>
+                  <div>
+                    {entry.date} <WorkIcon />
+                  </div>
+                  <div>{entry.description}</div>
+                  {entry.diagnosisCodes ? (
+                    <ul>
+                      {entry.diagnosisCodes.map((code) => {
+                        return (
+                          <li key={code}>
+                            {code} {diagnoses[code].name}
+                          </li>
+                        );
+                      })}
+                    </ul>
+                  ) : null}
+                  diagnose by {entry.specialist}
+                </div>
+              );
+            case 'HealthCheck':
+              return (
+                <div key={entry.id}>
+                  <div>
+                    {entry.date} <MedicalServicesIcon />
+                  </div>
+                  <div>{entry.description}</div>
+                  {entry.diagnosisCodes ? (
+                    <ul>
+                      {entry.diagnosisCodes.map((code) => {
+                        return (
+                          <li key={code}>
+                            {code} {diagnoses[code].name}
+                          </li>
+                        );
+                      })}
+                    </ul>
+                  ) : null}
+                  diagnose by {entry.specialist}
+                </div>
+              );
+            default:
+              return 'hi';
+          }
         })}
       </div>
     );
